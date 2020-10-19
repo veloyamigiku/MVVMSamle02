4125167e79dc5c61329e55109640ffc1ca995ed7
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-10-19 20:56:29 +0900

    ・import文を最適化する。 ・メイン画面をレイアウトする。

--- app/src/main/java/jp/co/myself/mvvmsample02/MainActivity.java
+++ app/src/main/java/jp/co/myself/mvvmsample02/MainActivity.java
@@ -1,14 +1,68 @@
 package jp.co.myself.mvvmsample02;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int VIEW_ID_RECYCLERVIEW = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // レイアウトを作成する。
+        ConstraintLayout cl = new ConstraintLayout(this);
+        cl.setLayoutParams(new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.MATCH_PARENT));
+        cl.setBackgroundColor(Color.BLUE);
+        setContentView(cl);
+
+        // コンポーネントを作成＆配置する。
+        RecyclerView rv = new RecyclerView(this);
+        rv.setId(VIEW_ID_RECYCLERVIEW);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        cl.addView(rv);
+        ConstraintSet rvCs = new ConstraintSet();
+        rvCs.clone(cl);
+        rvCs.constrainWidth(
+                rv.getId(),
+                ConstraintSet.MATCH_CONSTRAINT);
+        rvCs.constrainDefaultHeight(
+                rv.getId(),
+                ConstraintSet.MATCH_CONSTRAINT);
+        rvCs.connect(
+                rv.getId(),
+                ConstraintSet.TOP,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.TOP,
+                0);
+        rvCs.connect(
+                rv.getId(),
+                ConstraintSet.LEFT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.LEFT,
+                0);
+        rvCs.connect(
+                rv.getId(),
+                ConstraintSet.RIGHT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.RIGHT,
+                0);
+        rvCs.connect(
+                rv.getId(),
+                ConstraintSet.BOTTOM,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.BOTTOM,
+                0);
+        rvCs.applyTo(cl);
+
     }
 }
--- app/src/test/java/jp/co/myself/mvvmsample02/ExampleUnitTest.java
+++ app/src/test/java/jp/co/myself/mvvmsample02/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package jp.co.myself.mvvmsample02;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
