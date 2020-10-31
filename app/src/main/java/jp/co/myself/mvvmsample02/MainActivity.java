0bb32b4d1330577c4d31e476f9af0b7418dd8981
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-10-31 22:16:02 +0900

    ・ToolBarを導入する。

--- app/src/main/java/jp/co/myself/mvvmsample02/MainActivity.java
+++ app/src/main/java/jp/co/myself/mvvmsample02/MainActivity.java
@@ -1,9 +1,12 @@
 package jp.co.myself.mvvmsample02;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,7 +14,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int VIEW_ID_RECYCLERVIEW = 1;
+    private static final int VIEW_ID_TOOLBAR = 1;
+    private static final int VIEW_ID_RECYCLERVIEW = 2;
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +37,50 @@ public class MainActivity extends AppCompatActivity {
         cl.setLayoutParams(new ConstraintLayout.LayoutParams(
                 ConstraintLayout.LayoutParams.MATCH_PARENT,
                 ConstraintLayout.LayoutParams.MATCH_PARENT));
-        cl.setBackgroundColor(Color.BLUE);
+        //cl.setBackgroundColor(Color.BLUE);
         setContentView(cl);
+
+        Toolbar tb = new Toolbar(this);
+        tb.setId(VIEW_ID_TOOLBAR);
+        tb.setTitle("MVVMSample1234");
+        TypedValue colorPrimaryTv = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, colorPrimaryTv, true);
+        tb.setBackgroundColor(colorPrimaryTv.data);
+        TypedValue colorOnPrimaryTv = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorOnPrimary, colorOnPrimaryTv, true);
+        tb.setTitleTextColor(colorOnPrimaryTv.data);
+        tb.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_name));
+        cl.addView(tb);
+        ConstraintSet tbCs = new ConstraintSet();
+        tbCs.clone(cl);
+        tbCs.constrainWidth(
+                tb.getId(),
+                ConstraintSet.MATCH_CONSTRAINT);
+        tbCs.constrainHeight(
+                tb.getId(),
+                ConstraintSet.WRAP_CONTENT);
+        tbCs.connect(
+                tb.getId(),
+                ConstraintSet.TOP,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.TOP,
+                0);
+        tbCs.connect(
+                tb.getId(),
+                ConstraintSet.LEFT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.LEFT,
+                0);
+        tbCs.connect(
+                tb.getId(),
+                ConstraintSet.RIGHT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.RIGHT,
+                0);
+        tbCs.applyTo(cl);
+
+        setSupportActionBar(tb);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // コンポーネントを作成＆配置する。
         RecyclerView rv = new RecyclerView(this);
@@ -41,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         rvCs.connect(
                 rv.getId(),
                 ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
+                tb.getId(),
                 ConstraintSet.TOP,
                 0);
         rvCs.connect(
